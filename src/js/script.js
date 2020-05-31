117222816b19adeba0521fb6d6a13c3fd6f2f439
Zorin K <zosya43nax@gmail.com>
2020-05-31 23:48:01 +0300

    catalog link event began

--- src/js/script.js
+++ src/js/script.js
@@ -32,3 +32,22 @@ $(document).ready(function () {
 // document.querySelector(".slider-prev").addEventListener("click", () => {
 //   slider.goTo("prev");
 // });
+/* catalog button event */
+window.addEventListener("DOMContentLoaded", () => {
+  const moreLink = document.querySelector(".catalog-link");
+  const backLink = document.querySelector(".back");
+  const prodWrapper = document.querySelector(".catalog-content-item-prodWrapper");
+  const more = document.querySelector(".catalog-content-item-more");
+  const item = document.querySelector(".catalog-content-item");
+
+  moreLink.addEventListener("click", (e) => {
+    e.preventDefault();
+    item.classList.remove(prodWrapper);
+    item.classList.add(more);
+  });
+  backLink.addEventListener("click", (e) => {
+    e.preventDefault();
+    item.classList.remove(more);
+    item.classList.add(prodWrapper);
+  });
+});
