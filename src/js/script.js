5cdd64cec3595f571f30adaf664b3090aebc19dc
Zorin K <zosya43nax@gmail.com>
2020-06-11 13:29:09 +0300

    smooth scroll

--- src/js/script.js
+++ src/js/script.js
@@ -144,4 +144,19 @@ $(document).ready(function () {
     });
     return false;
   });
+  /* Smooth scroll */
+  // fadeIn icon
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 1550) {
+      $(".scroll").fadeIn("2s");
+    } else {
+      $(".scroll").fadeOut("2s");
+    }
+  });
+  //scroll
+  $("a[href^='#']").click(function () {
+    const _href = $(this).attr("href");
+    $("html, body").animate({ scrollTop: $(_href).offset().top + "px" });
+    return false;
+  });
 });
